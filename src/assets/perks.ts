d49554ee9c10e44a55e2fe95e3e71681d0e118b2
EduardIonescu <37626215+EduardIonescu@users.noreply.github.com>
2025-01-29 13:06:01 +0100

    champion and perks images #24

---
+++ src/assets/perks.ts
@@ -0,0 +1,138 @@
+export default [
+  {
+    id: 8100,
+    icon: "perk-images/7200_Domination.png",
+    name: "Domination",
+    runes: [
+      {
+        id: 8112,
+        key: "Electrocute",
+        icon: "perk-images/Domination/Electrocute/Electrocute.png",
+        name: "Electrocute",
+      },
+      {
+        id: 8128,
+        key: "DarkHarvest",
+        icon: "perk-images/Domination/DarkHarvest/DarkHarvest.png",
+        name: "Dark Harvest",
+      },
+      {
+        id: 9923,
+        key: "HailOfBlades",
+        icon: "perk-images/Domination/HailOfBlades/HailOfBlades.png",
+        name: "Hail of Blades",
+      },
+    ],
+  },
+  {
+    id: 8300,
+
+    icon: "perk-images/7203_Whimsy.png",
+    name: "Inspiration",
+
+    runes: [
+      {
+        id: 8351,
+        key: "GlacialAugment",
+        icon: "perk-images/Inspiration/GlacialAugment/GlacialAugment.png",
+        name: "Glacial Augment",
+      },
+      {
+        id: 8360,
+        key: "UnsealedSpellbook",
+        icon: "perk-images/Inspiration/UnsealedSpellbook/UnsealedSpellbook.png",
+        name: "Unsealed Spellbook",
+      },
+      {
+        id: 8369,
+        key: "FirstStrike",
+        icon: "perk-images/Inspiration/FirstStrike/FirstStrike.png",
+        name: "First Strike",
+      },
+    ],
+  },
+  {
+    id: 8000,
+
+    icon: "perk-images/7201_Precision.png",
+    name: "Precision",
+
+    runes: [
+      {
+        id: 8005,
+        key: "PressTheAttack",
+        icon: "perk-images/Precision/PressTheAttack/PressTheAttack.png",
+        name: "Press the Attack",
+      },
+      {
+        id: 8008,
+        key: "LethalTempo",
+        icon: "perk-images/Precision/LethalTempo/LethalTempoTemp.png",
+        name: "Lethal Tempo",
+      },
+      {
+        id: 8021,
+        key: "FleetFootwork",
+        icon: "perk-images/Precision/FleetFootwork/FleetFootwork.png",
+        name: "Fleet Footwork",
+      },
+      {
+        id: 8010,
+        key: "Conqueror",
+        icon: "perk-images/Precision/Conqueror/Conqueror.png",
+        name: "Conqueror",
+      },
+    ],
+  },
+  {
+    id: 8400,
+    icon: "perk-images/7204_Resolve.png",
+    name: "Resolve",
+
+    runes: [
+      {
+        id: 8437,
+        key: "GraspOfTheUndying",
+        icon: "perk-images/Resolve/GraspOfTheUndying/GraspOfTheUndying.png",
+        name: "Grasp of the Undying",
+      },
+      {
+        id: 8439,
+        key: "Aftershock",
+        icon: "perk-images/Resolve/VeteranAftershock/VeteranAftershock.png",
+        name: "Aftershock",
+      },
+      {
+        id: 8465,
+        key: "Guardian",
+        icon: "perk-images/Resolve/Guardian/Guardian.png",
+        name: "Guardian",
+      },
+    ],
+  },
+  {
+    id: 8200,
+    icon: "perk-images/7202_Sorcery.png",
+    name: "Sorcery",
+    runes: [
+      {
+        id: 8214,
+        key: "SummonAery",
+        icon: "perk-images/Sorcery/SummonAery/SummonAery.png",
+        name: "Summon Aery",
+      },
+      {
+        id: 8229,
+        key: "ArcaneComet",
+        icon: "perk-images/Sorcery/ArcaneComet/ArcaneComet.png",
+        name: "Arcane Comet",
+      },
+      {
+        id: 8230,
+        key: "PhaseRush",
+        icon: "perk-images/Sorcery/PhaseRush/PhaseRush.png",
+        name: "Phase Rush",
+      },
+    ],
+  },
+];
