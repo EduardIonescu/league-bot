b94e97e5294637dcfc0f2d2d2a6cb17008b1de9d
EduardIonescu <37626215+EduardIonescu@users.noreply.github.com>
2025-01-30 21:23:44 +0100

    add mel

--- src/assets/champions.ts
+++ src/assets/champions.ts
@@ -81,6 +81,7 @@ export default [
   { id: 90, name: "Malzahar", lanes: ["mid", "support"] },
   { id: 57, name: "Maokai", lanes: ["top", "support"] },
   { id: 11, name: "MasterYi", lanes: ["jungle"] },
+  { id: 800, name: "Mel", lanes: ["support", "bot", "mid"] },
   { id: 902, name: "Milio", lanes: ["support"] },
   { id: 21, name: "MissFortune", lanes: ["bot", "support"] },
   { id: 82, name: "Mordekaiser", lanes: ["top", "jungle"] },
