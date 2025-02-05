539f17501b190af7eb582265a578e40d0f473d19
EduardIonescu <37626215+EduardIonescu@users.noreply.github.com>
2025-02-05 19:59:49 +0100

    remove unused types #5

--- src/lib/types/common.ts
+++ src/lib/types/common.ts
@@ -1,5 +1,5 @@
 import { UserAdvanced } from "../../data/schema.js";
-import { Account, Perks, Region, SpectatorParticipant } from "./riot.js";
+import { Account, SpectatorParticipant } from "./riot.js";
 
 export type RefundedBettingUser = {
   updatedUser: UserAdvanced;
@@ -12,36 +12,6 @@ export type WinnerBetingUser = {
 };
 export type Currencies = { tzapi: number; nicu: number };
 export type Currency = "nicu" | "tzapi";
-
-export type Bet = {
-  discordId: string;
-  amount: Currencies;
-  win: boolean;
-  timestamp: Date;
-  inGameTime: number;
-};
-
-export type SentIn = { channelId: string; messageIds: string[] }[];
-export type Match = {
-  gameId: number;
-  player: string;
-  gameType: string;
-  gameMode: string;
-  gameQueueConfigId: number;
-  summonerId: string;
-  inGameTime: number;
-  gameStartTime: number;
-  region: Region;
-  sentIn: SentIn;
-  againstBot: boolean;
-  bets: Bet[];
-};
-
-export type FinishedMatch = Match & {
-  win: boolean | "remake";
-  participants: FinishedMatchParticipant[];
-  gameDuration: number;
-};
 
 export type AmountByUser = {
   discordId: string;
@@ -60,34 +30,6 @@ export type SummonerSpell = {
   name: string;
   id: string;
   weights: { [key in Lane]?: number };
-};
-
-export type FinishedMatchParticipant = {
-  kills: number;
-  assists: number;
-  deaths: number;
-  totalDamageDealtToChampions: number;
-  teamPosition: string;
-  championId: number;
-  champLevel: number;
-  summoner1Id: number;
-  summoner2Id: number;
-  totalMinionsKilled: number;
-  // Jungle monsters + epic camps etc
-  neutralMinionsKilled: number;
-  item0: number;
-  item1: number;
-  item2: number;
-  item3: number;
-  item4: number;
-  item5: number;
-  item6: number;
-  perks: Perks;
-  puuid: string;
-  riotIdGameName: string;
-  riotIdTagline: string;
-  teamId: number;
-  win: boolean;
 };
 
 export type HTMLString = `<!DOCTYPE html>${string}`;
